7e26dbb9da83c3ff3946019b2e4027739476fcea
Jirapon6531503015 <122779647+Jirapon6531503015@users.noreply.github.com>
2023-09-04 14:55:34 +0000

    6531503015

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
